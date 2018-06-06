d6ebed0e3e9f104e5574acdef51c311a95fd772b
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-06-06 22:09:37 +0200

    hook service add_action -> $this->addAction, same for filters

--- includes/Service/ContactHookService.php
+++ includes/Service/ContactHookService.php
@@ -29,24 +29,24 @@ class ContactHookService extends AbstractHookService
          * Admin Hooks
          */
         //Menus
-        add_action('admin_menu', [$this, 'customizeMenus']);
+        $this->addAction('admin_menu', [$this, 'customizeMenus']);
 
         //Translate
-        add_action('plugins_loaded', [$this, 'loadTextdomain']);
+        $this->addAction('plugins_loaded', [$this, 'loadTextdomain']);
 
         //Send contact mail
-        add_action('wwp-contact.contact_handler_service_success', [$this, 'setupMailDelivery'], 10, 3); //You can comment this to disable email delivery to debug
+        $this->addAction('wwp-contact.contact_handler_service_success', [$this, 'setupMailDelivery'], 10, 3); //You can comment this to disable email delivery to debug
 
         //Save contact somewhere
-        add_action('wwp-contact.contact_handler_service_success', [$this, 'saveContact'], 10, 3); //You can comment this to disable contact getting persisted
+        $this->addAction('wwp-contact.contact_handler_service_success', [$this, 'saveContact'], 10, 3); //You can comment this to disable contact getting persisted
 
         //User deletion
         /** @var ContactUserDeleterService $deleterService */
         $deleterService = $this->manager->getService('userDeleter');
         //User deletion : on before confirmation screen
-        add_action('delete_user_form', [$deleterService, 'deleteUserForm'], 10, 2);
+        $this->addAction('delete_user_form', [$deleterService, 'deleteUserForm'], 10, 2);
         //User deletion : effective deletion
-        add_action('delete_user',[$deleterService,'onUserBeforeDelete']);
+        $this->addAction('delete_user',[$deleterService,'onUserBeforeDelete']);
 
         return $this;
     }
@@ -64,7 +64,7 @@ class ContactHookService extends AbstractHookService
         //Add entry under top-level functionalities menu
         $suffix = add_submenu_page('wonderwp-modules', 'Contact', 'Contact', WwpAdminChangerService::$DEFAULTMODULECAP, WWP_PLUGIN_CONTACT_NAME, $callable);
 
-        add_action("admin_print_scripts-$suffix", [$this, 'my_plugin_admin_scripts']);
+        $this->addAction("admin_print_scripts-$suffix", [$this, 'my_plugin_admin_scripts']);
     }
 
     public function my_plugin_admin_scripts()
