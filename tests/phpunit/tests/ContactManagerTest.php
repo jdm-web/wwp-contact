d9e999589c94c417312733f1b5b747a6e97fcbf1
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-07-31 18:39:07 +0200

    [Unit tests] Added some untested services to the test suite

--- tests/phpunit/tests/ContactManagerTest.php
+++ tests/phpunit/tests/ContactManagerTest.php
@@ -3,10 +3,12 @@
 namespace WonderWp\Plugin\Contact\Test\PhpUnit;
 
 use WonderWp\Component\Repository\PostRepository;
+use WonderWp\Plugin\Contact\Repository\ContactRepository;
 use WonderWp\Plugin\Contact\Service\ContactFormService;
 use WonderWp\Plugin\Contact\Service\ContactHandlerService;
 use WonderWp\Plugin\Contact\Service\ContactMailService;
 use WonderWp\Plugin\Contact\Service\ContactPersisterService;
+use WonderWp\Plugin\Contact\Service\ContactRgpdService;
 use WonderWp\Plugin\Contact\Service\ContactUserDeleterService;
 use WonderWp\Plugin\Contact\Service\Exporter\ContactCsvExporterService;
 use WonderWp\Plugin\Core\Test\ManagerTestCase;
@@ -26,12 +28,13 @@ class ContactManagerTest extends ManagerTestCase
         $parentDefinitions = parent::getServicesDefinitionsToTest();
 
         return $parentDefinitions + [
-                'form' => ContactFormService::class,
-                //'contactHandler' => ContactHandlerService::class,
-                'mail'=>ContactMailService::class,
-                'persister'=>ContactPersisterService::class,
-                //'exporter' =>ContactCsvExporterService::class,
-                'userDeleter' => ContactUserDeleterService::class
+                'form'              => ContactFormService::class,
+                'contactHandler'    => ContactHandlerService::class,
+                'mail'              => ContactMailService::class,
+                'persister'         => ContactPersisterService::class,
+                'exporter'          => ContactCsvExporterService::class,
+                'userDeleter'       => ContactUserDeleterService::class,
+                'rgpd'              => ContactRgpdService::class
             ];
     }
 
