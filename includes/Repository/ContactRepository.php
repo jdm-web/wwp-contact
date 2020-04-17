99892ca62c984ed5412570a6e69f8964e50f34a5
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-04-17 16:22:31 +0200

    new method to find contact message by data key and val

--- includes/Repository/ContactRepository.php
+++ includes/Repository/ContactRepository.php
@@ -19,30 +19,65 @@ class ContactRepository extends BaseRepository
         $qb
             ->where('m.data LIKE :mail')
             ->setParameter(':mail', '%' . $mail . '%')
-            ->orderBy('m.createdAt','DESC')
+            ->orderBy('m.createdAt', 'DESC')
         ;
 
         return $qb->getQuery()->getResult();
     }
 
-     /**
-      * @param string $mail
-      * @return ContactEntity[]
-      */
-     public function findMessagesForEmailAndIds($email, $contactIds)
-     {
-       $qb = $this->createQueryBuilder('contact');
-       $qb->where('contact.data LIKE :email')
-          ->andWhere('contact.id IN(:ids)')
-          ->setParameter(':email', '%' . $email . '%')
-          ->setParameter('ids', array_values($contactIds))
-       ;
-       return $qb->getQuery()->getResult();
-     }
+    /**
+     * @param string $mail
+     *
+     * @return ContactEntity[]
+     */
+    public function findMessagesForEmailAndIds($email, $contactIds)
+    {
+        $qb = $this->createQueryBuilder('contact');
+        $qb->where('contact.data LIKE :email')
+           ->andWhere('contact.id IN(:ids)')
+           ->setParameter(':email', '%' . $email . '%')
+           ->setParameter('ids', array_values($contactIds))
+        ;
 
-     public function findMessagesForExport(ContactFormEntity $contactFormEntity){
-         return $this->findBy([
-             'form' => $contactFormEntity,
-         ]);
-     }
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param ContactFormEntity $contactFormEntity
+     *
+     * @return array|mixed
+     */
+    public function findMessagesForExport(ContactFormEntity $contactFormEntity)
+    {
+        return $this->findBy([
+            'form' => $contactFormEntity,
+        ]);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed  $val
+     * @param int    $formId
+     *
+     * @return mixed
+     */
+    public function findByDataKeyVal($key, $val, $formId = null)
+    {
+        $serial = serialize($val);
+
+        $qb = $this->createQueryBuilder('m');
+        $qb
+            ->where('m.data LIKE :search')
+            ->setParameter(':search', '%' . $key . '";' . $serial . '%')
+            ->orderBy('m.createdAt', 'DESC')
+        ;
+        if (!empty($formId)) {
+            $qb
+                ->andWhere('m.form = :formId')
+                ->setParameter(':formId', $formId)
+            ;
+        }
+
+        return $qb->getQuery()->getResult();
+    }
 }
